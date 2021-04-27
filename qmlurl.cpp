4e4523ac895eef1b2fd410a6b0ba62753b3cc1cf
Oleg Blednov <blake-r@mail.ru>
2021-04-28 02:13:09 +0300

    Replace clipboard logic with a simple protocol comparison (#3)

--- qmlurl.cpp
+++ qmlurl.cpp
@@ -1,22 +1,7 @@
 #include <QUrl>
 #include "qmlurl.h"
 
-QString QmlUrl::siteTagFromUrl(const QString &text) const {
-    QUrl url = QUrl(text, QUrl::StrictMode);
-    if (!url.isValid() || url.isEmpty() || url.isLocalFile() || url.isRelative()) {
-        return text;
-    }
-    QStringList parts = url.host().split('.');
-    if (parts[0] == "www") {
-        // www.google.com -> google.com
-        parts.removeFirst();
-    }
-    if (parts.size() > 1) {
-        // google.com -> google
-        parts.removeLast();
-    }
-    if (parts.isEmpty()) {
-        return text;
-    }
-    return parts.last();
+QString QmlUrl::resolved(const QString &relativeUrl)
+{
+    return QUrl::resolved(QUrl(relativeUrl)).url();
 }
--- qmlurl.h
+++ qmlurl.h
@@ -1,12 +1,45 @@
 #ifndef QMLURL_H
 #define QMLURL_H
 #include <QObject>
+#include <QUrl>
 
-class QmlUrl : public QObject
+class QmlUrl : public QObject, QUrl
 {
     Q_OBJECT
+    Q_PROPERTY(QString url READ url WRITE setUrl)
+
+    Q_PROPERTY(bool isValid READ isValid)
+    Q_PROPERTY(QString errorString READ errorString)
+
+    Q_PROPERTY(bool isEmpty READ isEmpty)
+
+    Q_PROPERTY(QString scheme READ scheme WRITE setScheme)
+    Q_PROPERTY(QString authority READ authority WRITE setAuthority)
+    Q_PROPERTY(QString userInfo READ userInfo WRITE setUserInfo)
+    Q_PROPERTY(QString userName READ userName WRITE setUserName)
+    Q_PROPERTY(QString password READ password WRITE setPassword)
+    Q_PROPERTY(QString host READ host WRITE setHost)
+    Q_PROPERTY(int port READ port WRITE setPort)
+    Q_PROPERTY(QString path READ path WRITE setPath)
+
+    Q_PROPERTY(QString fileName READ fileName)
+
+    Q_PROPERTY(bool hasQuery READ hasQuery)
+    Q_PROPERTY(QString query READ query WRITE setQuery)
+
+    Q_PROPERTY(bool hasFragment READ hasFragment)
+    Q_PROPERTY(QString fragment READ fragment WRITE setFragment)
+
+    Q_PROPERTY(bool isRelative READ isRelative)
+
+    Q_PROPERTY(bool isLocalFile READ isLocalFile)
+    Q_PROPERTY(QString toLocalFile READ toLocalFile)
+
+    Q_PROPERTY(QString toDisplayString READ toDisplayString)
+    Q_PROPERTY(QString toString READ toString)
+
 public:
-    Q_INVOKABLE QString siteTagFromUrl(const QString &text) const;
+    Q_INVOKABLE QString resolved(const QString &relativeUrl);
 };
 
 #endif // QMLURL_H
